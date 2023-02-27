654aa8bebb37f23df91cc0318e5a5a4c4befa451
Trehub Anatolii <tridfoto@gmail.com>
2023-02-27 22:04:17 +0200

    correct package

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,5 +2,22 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 console.log(galleryItems);
-console.log("all works");
+
+
+const divGallery = document.querySelector('.gallery');
+galleryItems.forEach(el=>{
+    divGallery.insertAdjacentHTML('afterbegin',
+    `<div class="gallery__item">
+        <a class="gallery__link" href=${el.original}>
+            <img class="gallery__image" src=${el.preview} alt="${el.description}"/>
+        </a>
+    </div>`
+    )
+    
+});
+
+let simpleGallery = new SimpleLightbox('.gallery__link',{captionsData:'alt', captionDelay:250});
+    simpleGallery.on('show.simplelightbox');
