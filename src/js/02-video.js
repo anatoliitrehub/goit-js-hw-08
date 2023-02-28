dd0318d98e28bc4afcafc76df109ffc2443dc433
Trehub Anatolii <tridfoto@gmail.com>
2023-02-28 17:02:58 +0200

    sent files

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,25 @@
+import VimeoPlayer from '@vimeo/player';
+
+const iframe = document.querySelector('iframe');
+const player = new VimeoPlayer(iframe);
+import throttle from 'lodash.throttle';
+
+let startTime = localStorage.getItem("videoplayer-current-time")?localStorage.getItem("videoplayer-current-time"):0;
+
+player.setCurrentTime(startTime).then(function(seconds) {
+    // console.log(seconds);
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});
+
+player.on('timeupdate', throttle((evt)=>localStorage.setItem("videoplayer-current-time",evt.seconds),1000));
+
 
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,48 @@
+import throttle from 'lodash.throttle';
 
+const feedForm = document.querySelector('.feedback-form');
+
+feedForm.addEventListener('input',throttle(formHandler,500));
+feedForm.addEventListener('submit',submitHandler);
+let formData = {
+    "email":'',
+    "message":''
+   };
+  
+if(localStorage.getItem("feedback-form-state")){
+    const localData = JSON.parse(localStorage.getItem("feedback-form-state"));
+    const {email,message} = localData;
+    formData = {
+        "email":email,
+        "message":message
+    };
+    feedForm.email.value = email;
+    feedForm.message.value = message;
+}
+
+function formHandler({target:{name,value}}){
+
+    formData = {
+    ...formData,
+        [name] : value,
+
+   };
+// console.log(formData);
+localStorage.setItem("feedback-form-state",JSON.stringify(formData));
+}
+
+function submitHandler(evt){
+    evt.preventDefault();
+
+    if(!formData.email || !formData.message) {
+        alert("all areas must been fill");
+        return; }
+    console.log(formData);
+    localStorage.removeItem("feedback-form-state");
+    
+    feedForm.reset();
+    formData = {
+        "email":"",
+        "message":""
+    };
+}
